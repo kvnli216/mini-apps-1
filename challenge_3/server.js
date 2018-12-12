9f23bfb1a6591ea79aeb3808e05c4f9c7f5cb86d
Kevin Li <li.ctrl.z@gmail.com>
2018-12-12 11:49:57 -0800

    Implement boilerplate for server + cient

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
 
+const port = 1337;
+
+
+// Middleware
+app.use(morgan('tiny'));
+app.use(express.static('public'));
+
+// Request Handling
+
+
+// 404
+app.use((req, res, next) => {
+  res.status(404);
+  res.send({error: 'Not Found' });
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
 
