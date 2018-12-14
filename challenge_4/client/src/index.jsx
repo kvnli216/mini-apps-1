58315e72ccf35794fa0b34f18c1c9a74c001619f
Kevin Li <li.ctrl.z@gmail.com>
2018-12-13 20:04:11 -0800

    Implement horizontal win check

--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -18,8 +18,68 @@ class App extends React.Component {
       ],
       playerOneTurn: true
     };
+
+    this.togglePlayerTurn = this.togglePlayerTurn.bind(this);
+    this.updateBoard = this.updateBoard.bind(this);
+    this.horizontalWin = this.horizontalWin.bind(this);
   }
   
+  // ------------------------------------------------
+  //                    State Utility
+  // ------------------------------------------------
+  updateBoard(newBoard) {
+    this.setState(state => {
+      return {
+        board: newBoard
+      }
+    });
+  }
+
+  togglePlayerTurn() {
+    this.setState(state => {
+      return {
+        playerOneTurn: !this.state.playerOneTurn
+      }
+    });
+  }
+
+  // ------------------------------------------------
+  //              Checking End Condition
+  // ------------------------------------------------
+
+  horizontalWin() {
+    console.log('running win check');
+    let board = this.state.board;
+    this.state.board.forEach((col, indexC) => { // iterate through every column
+      col.forEach((slot, indexS) => { // iterate through slots of a column
+        if (slot !== '') { // if a piece exists here
+          let piece = slot;
+          let isWinner = true;
+          // check piece match for next 3 columns
+          for (let i = 1; i < 4; i++) {
+            if (board[indexC + i][indexS] !== piece) {
+              isWinner = false;
+              i = 4;
+            }
+          }
+          if (isWinner) {
+            if (this.state.playerOneTurn) {
+              alert('Player 1 Wins!');
+            } else {
+              alert('Player 2 Wins!');
+            }
+          }
+          return isWinner; // Todo: update scoreboard
+        }
+      });
+    });
+  }
+
+
+  // ------------------------------------------------
+  //                   Controller
+  // ------------------------------------------------
+
   selectCol(e) {
     e.preventDefault();
     let slotChoice = Number(e.target.parentElement.id); // index of col
@@ -34,13 +94,9 @@ class App extends React.Component {
         i = 0;
       }
     }
-    
-    this.setState(state => {
-      return {
-        board: newBoard,
-        playerOneTurn: !this.state.playerOneTurn
-      }
-    });
+    this.togglePlayerTurn();
+    this.updateBoard(newBoard);
+    this.horizontalWin();
   }
 
   render() {
